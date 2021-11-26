0a1256ddfc3c366325a7292620632e93f5382446
Yesid Varela <yleandrovarela@gmail.com>
2021-11-26 14:27:03 -0500

    Create Routes and Styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { NavLink, Routes, Route } from "react-router-dom";
 
-function App() {
+//Components
+import Inicio from "./components/Inicio";
+import Blog from "./components/Blog";
+import Tienda from "./components/Tienda";
+import Error404 from "./components/Error404";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Menu>
+        <NavLink to="/">Inicio</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="/tienda">Tienda</NavLink>
+      </Menu>
+
+      <main>
+        <Routes>
+          <Route path="/" exact={true} component={Inicio} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/tienda" component={Tienda} />
+          <Route component={Error404} />
+        </Routes>
+      </main>
+
+      <aside>
+        <h3>Sidebar</h3>
+      </aside>
+    </Contenedor>
   );
-}
+};
+
+const Contenedor = styled.div`
+  max-width: 1000px;
+  padding: 40px;
+  width: 90%;
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 2fr 1fr;
+  background: #fff;
+  margin: 40px 0;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+
+const Menu = styled.nav`
+  width: 100%;
+  text-align: center;
+  background: #092c4c;
+  grid-column: span 2;
+  border-radius: 3px;
+
+  a {
+    color: #fff;
+    display: inline-block;
+    padding: 15px 20px;
+  }
+
+  a:hover {
+    background: #1d85e8;
+    text-decoration: none;
+  }
+`;
 
 export default App;
